0e1fc8a58d4dfcb4698faeeb9c9371f794c1c5e6
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-08 19:25:19 +0530

    added getbyidquery in handler

--- src/services/CommunityConnect/Features/Admin/Queries/GetEventById/GetEventByIdQueryEndPoint.cs
+++ src/services/CommunityConnect/Features/Admin/Queries/GetEventById/GetEventByIdQueryEndPoint.cs
@@ -1,5 +1,13 @@
-﻿namespace CommunityConnect.Features.Admin.Queries.GetEventById
+﻿using CommunityConnect.Models;
+using MediatR;
+
+namespace CommunityConnect.Features.Admin.Queries.GetEventById
 {
+    public class GetEventByIdQuery : IRequest<Event>
+    {
+        public int EventId { get; set; }
+    }
+    
     public class GetEventByIdQueryEndPoint
     {
     }
