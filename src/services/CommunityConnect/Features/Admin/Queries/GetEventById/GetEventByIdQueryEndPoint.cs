38074cc1356aef23ca70ad76c77479c13be21292
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-08 19:30:55 +0530

    GetEventByIdQueryEndPoint Started and extended ICarterModule.

--- src/services/CommunityConnect/Features/Admin/Queries/GetEventById/GetEventByIdQueryEndPoint.cs
+++ src/services/CommunityConnect/Features/Admin/Queries/GetEventById/GetEventByIdQueryEndPoint.cs
@@ -1,11 +1,16 @@
-﻿using CommunityConnect.Models;
+﻿using Carter;
+using CommunityConnect.Models;
 using MediatR;
 
 namespace CommunityConnect.Features.Admin.Queries.GetEventById
 {
-    
-    
-    public class GetEventByIdQueryEndPoint
+
+
+    public class GetEventByIdQueryEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
