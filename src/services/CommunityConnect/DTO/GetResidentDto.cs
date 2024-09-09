294701d2ee342cd84694c56def1068d7a0452cf3
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-09 11:36:10 +0530

    changes made

--- src/services/CommunityConnect/DTO/GetResidentDto.cs
+++ src/services/CommunityConnect/DTO/GetResidentDto.cs
@@ -2,6 +2,7 @@
 {
     public class GetResidentDto
     {
+        public string Id {  get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string FlatNo { get; set; }
