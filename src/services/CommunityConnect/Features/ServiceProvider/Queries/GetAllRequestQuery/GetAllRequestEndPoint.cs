fcd0262349f49531f37d54155e3b4ea59db1fb1e
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-10 17:28:02 +0530

    changes made in getallrequests in which if serviceprovider wants particular resident he can do that or he can get all the services.

--- src/services/CommunityConnect/Features/ServiceProvider/Queries/GetAllRequestQuery/GetAllRequestEndPoint.cs
+++ src/services/CommunityConnect/Features/ServiceProvider/Queries/GetAllRequestQuery/GetAllRequestEndPoint.cs
@@ -1,5 +1,6 @@
 ﻿using Carter;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 
 namespace CommunityConnect.Features.ServiceProvider.Queries.GetAllRequestQuery
 {
@@ -7,7 +8,7 @@ namespace CommunityConnect.Features.ServiceProvider.Queries.GetAllRequestQuery
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/Service/requests/{residentId}", async (string? residentId, IMediator mediator) =>
+            app.MapGet("/Service/requests/{residentId?}", [Authorize(Roles = "ServiceProvider")]  async (string? residentId, IMediator mediator) =>
             {
                 var result = await mediator.Send(new GetAllServiceRequestsQuery { ResidentId = residentId });
                 return result != null ? Results.Ok(result) : Results.NotFound();
