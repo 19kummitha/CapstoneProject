9585ab5dcba881f99f69672cfc7439c5dee5efcc
VenkataKotiReddy.Kummitha@ust.com <VenkataKotiReddy.Kummitha@ust.com>
2024-09-12 12:15:17 +0530

    updated updatevent

--- src/services/CommunityConnect/Features/Admin/Commands/UpdateEventCommand/UpdateEventCommandHandler.cs
+++ src/services/CommunityConnect/Features/Admin/Commands/UpdateEventCommand/UpdateEventCommandHandler.cs
@@ -24,7 +24,6 @@ namespace CommunityConnect.Features.Admin.Commands.UpdateEventCommand
 
             if (eventToUpdate == null)
             {
-                // Event not found
                 return false;
             }
 
